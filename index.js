f290a11691c7d4f7d552776704ecc782dd86fc9e
OscarFabiani <spartan539@yahoo.com>
2019-06-18 11:41:20 -0700

    Added comments for new shorturl post route callback.

--- index.js
+++ index.js
@@ -37,39 +37,64 @@ var Url = mongoose.model('Url', urlSchema);
 
 
 
-//COMPARE WITH EXAMPLE AND OPTIMIZE (POSSIBLY ELIMINATE URL MODULE)
 app.route("/api/shorturl/new")
 .post(function(req, res) {
+
+  //variable for url property of body property of request object
   var url = req.body.url;
+
+  //checks if the url starts with http(s):// and sends an error response if not
   if (!url.match(/^https?:\/\/..*/i)) {
     res.json({error: "Invalid URL"});
   } else {
+
+    //variable for URL object
     var href = new URL(url);
+
+    //Uses dns module to test the hostname property of the href URL object and sends an error
+    //response if the hostname is unresolved
     dns.lookup(href.hostname, function(err) {
       if (err) {
         res.json({error: "Invalid Hostname"});
       } else {
+
+        //checks if the database collection already contains a document for the url 
         Url.find({original_url: url}, function(err, docs) {
           if (err) {console.log('err2: ' + err)};
+
+          //checks if the document array returned is empty (no matching document for url)
           if (docs == '') {
+
+            //counts the documents in the database collection
             Url.countDocuments(function(err, count) {
               if (err) {console.log('err1: ' + err)};
               console.log('count: ' + count);
+
+              //variable for json response and new Url document
               var doc = {original_url: url, short_url: count + 1};
+
+              //creates a new Url document and saves it to the database collection
               new Url(doc)
               .save(function (err, newDoc) {
                 if (err) { console.log('err3: ' + err)};
                 console.log('newDoc: ' + newDoc);
                 console.log('doc:');
                 console.log(doc);
+
+                //sends a respopnse containing the doc object (simplified document information
+                //omitting _id and __v properties)
                 res.json(doc);
               })
             })
           } else {
             console.log('already exists: ' + docs);
+
+            //variable for json response
             var doc = {original_url: url, short_url: docs[0].short_url};
             console.log('doc:');
             console.log(doc);
+
+            //sends a response containing simplified information for the existing document
             res.json(doc);
           }
         })
@@ -86,7 +111,7 @@ app.route("/api/shorturl/:short")
   var short = req.params.short;
   console.log('short: ' + short);
   Url.findOne({short_url: short}, function(err, doc) {
-    if (err) {console.log('erer: ' + err)};
+    if (err) {console.log('err: ' + err)};
     if (doc == null) {
       res.json({error: "No short url found for given input"});
     } else {
@@ -94,6 +119,13 @@ app.route("/api/shorturl/:short")
       res.redirect(doc.original_url);
     }
   })
+});
+
+
+// Answer not found to all the wrong routes
+app.use(function(req, res, next){
+  res.status(404);
+  res.type('txt').send('Not found');
 });
 
 
@@ -106,8 +138,8 @@ app.listen(3000, function () {
 
 
 //WRITE COMMENTS FOR ROUTES
-//FINISH OPTIMIZING ROUTES
 //EXPLORE CHANGING FILE STRUCTURE
+//EXPLORE CONVERTING CALLBACKS TO PROMISES
 //POSSIBLY REWORK INDEX.HTML AND STYLE.CSS
 
 
